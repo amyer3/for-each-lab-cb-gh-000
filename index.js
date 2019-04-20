e97ce7f24b813924f34a643487879b74e96234b3
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-20 17:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function iterativeLog(array){
   array.forEach((index, element) => console.log(`${index}: ${element}`);)
 }
+
+function iterate(callback){
+  
+}
+
+function doToArray(array, callback){
+  
+}
